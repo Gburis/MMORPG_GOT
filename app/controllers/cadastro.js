2bb5570b6189fc674263fc9f8b1b8648b27acefd
Gburis <gabrielleiteolimpio@gmail.com>
2019-08-12 10:45:24 -0300

    ajustes de rotas, controllers e validação de dados da tela de cadastro

---
+++ app/controllers/cadastro.js
@@ -0,0 +1,20 @@
+module.exports.cadastro = function(application, req, res){
+    res.render('cadastro', {validacao: {}, values: {}});
+}
+
+module.exports.cadastrar = function(application, req, res){
+    let dados = req.body;
+    req.assert('nome', 'O nome não pode estar vazio').notEmpty();
+    req.assert('usuario', 'O usuario não pode estar vazio').notEmpty();
+    req.assert('senha', 'O a senha não pode estar vazia').notEmpty();
+    req.assert('casa', 'Escolha uma casa').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if(errors){
+        res.render('cadastro', {validacao: errors, values: dados});
+        return false;
+    }else{
+        res.send('cadastrado');
+    }
+}
